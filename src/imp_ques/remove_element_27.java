aa854655a37c7b2b9a110cf983ab8026b60ddd5f
Priyanshu <priyanshukuamr.earth@gmail.com>
2026-01-29 20:20:40 +0530

    updated the solution for remove_element_27

--- src/imp_ques/remove_element_27.java
+++ src/imp_ques/remove_element_27.java
@@ -13,9 +13,29 @@ Explanation: Your function should return k = 2, with the first two elements of n
 It does not matter what you leave beyond the returned k (hence they are underscores).  */
 
 package imp_ques;
-
+import java.util.Arrays;
+import java.util.Scanner;
 public class remove_element_27 {
-        public int removeElement(int[] nums, int val) {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int[] nums = new int[size];
+        System.out.println("Enter the elements of the array: ");
+        for(int i = 0; i<nums.length; i++){
+            nums[i] = sc.nextInt();
+        }
+        System.out.println("Enter the target value: ");
+        int value = sc.nextInt();
+        int k = removeElement(nums, value);
+        int[] arr = new int[k];
+        for(int j = 0; j<k; j++){
+            arr[j] = nums[j];
+        }
+        System.out.println("After removing the elements the new array is: ");
+        System.out.println(Arrays.toString(arr));
+    }
+        public static int removeElement(int[] nums, int val) {
             int k = 0;
             for(int i = 0; i<nums.length; i++){
                 if(nums[i]!=val){
