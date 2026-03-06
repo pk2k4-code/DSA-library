85ea5c2b4efe6340e30a6adfb44550059c5ac4e7
Priyanshu <priyanshukumar.earth@gmail.com>
2026-03-06 19:13:44 +0530

    new questions added

---
+++ src/imp_ques/leetcode_410.java
@@ -0,0 +1,54 @@
+/*Given an integer array nums and an integer k, split nums into k non-empty subarrays such that the largest sum of any subarray is minimized.
+Return the minimized largest sum of the split.
+A subarray is a contiguous part of the array.
+
+Example 1:
+Input: nums = [7,2,5,10,8], k = 2
+Output: 18
+Explanation: There are four ways to split nums into two subarrays.
+The best way is to split it into [7,2,5] and [10,8], where the largest sum among the two subarrays is only 18.
+
+Example 2:
+Input: nums = [1,2,3,4,5], k = 2
+Output: 9
+Explanation: There are four ways to split nums into two subarrays.
+The best way is to split it into [1,2,3] and [4,5], where the largest sum among the two subarrays is only 9.
+ */
+
+
+
+package imp_ques;
+public class leetcode_410 {
+    public int splitArray(int[] nums, int k) {
+        int start = 0;
+        int end = 0;
+
+        for (int num : nums) {
+            start = Math.max(start, num);
+            end += num;
+        }
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            int pieces = 1;
+            int currentSum = 0;
+            for (int num : nums) {
+                if (currentSum + num > mid) {
+                    currentSum = num;
+                    pieces++;
+                } else {
+                    currentSum += num;
+                }
+            }
+
+            if (pieces <= k) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
+}
